9c3d97e46060e8d9a9a86dd0f232638c3169326a
Flirnz <flienardy73@gmail.com>
2025-12-05 21:53:45 +0100

    fist commit

---
+++ Dev/learning/Java/Brocode1/Lesson1_Variables/Variables/src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main (String[] args){
+        String name = "Ferdinand";
+        int age = 24;
+        char gender = 'm';
+        boolean isAdmin = true;
+        double pi = 3.14159;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(isAdmin);
+        System.out.println(pi);
+
+        System.out.println("Hallo my name is " + name + ", i am " + age + " years old, and i am a " + gender);
+        System.out.println("The Value of PI is" + pi);
+    }
+}
